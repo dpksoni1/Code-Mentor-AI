3ccac2d470b30dc125a12a0c7635ec4bbe1bfbce
dpksoni1 <101348315+dpksoni1@users.noreply.github.com>
2025-03-05 00:50:13 +0530

    Delete backend directory

--- backend/routes/ai.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const {getReview, getchat,}=require("../controllers/ai.controller")
-const router = express.Router();
-
-
-router.post("/get-review", getReview )
-router.post("/chat",getchat)
-
-module.exports = router;    
--- backend/routes/executeroute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {execute}=require("../controllers/codecontroller")
-const executerouter = express.Router();
-
-executerouter.post("/execute",execute)
-
-
-module.exports = executerouter;
